9e39026a40cdd4bfedde861b990f767b6b596cba
Y.Naito <tsukudani710@gmail.com>
2024-09-23 11:53:20 +0900

    feat: chap2 entering a row mode

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,22 @@
 use std::io::{self, Read, Write };
+use crossterm::terminal::{enable_raw_mode, disable_raw_mode};
+
 
 fn main() {
     let mut out = io::stdout();
+    if let Err(e) = enable_raw_mode() {
+        println!("failed to start a row mode");
+        panic!("{}", e);
+    }
     for b in io::stdin().bytes() {
-        let c = b.unwrap();
-        writeln!(&mut out ,"{}", c).unwrap();
+        let c = b.unwrap() as char;
+        if c ==  'q' {
+            break;
+        }
+        writeln!(&mut out ,"{}\n", c).unwrap();
+    }
+    if let Err(e) = disable_raw_mode() {
+        writeln!(&mut out, "failed to stop a row mod\r\n").unwrap();
+        panic!("{}", e);
     }
 }
