0f381b86ce4cd003c9a37c6595826be0b5967b23
Y.Naito <tsukudani710@gmail.com>
2024-09-23 12:00:21 +0900

    feat: chap2 Display Keypresses

--- src/main.rs
+++ src/main.rs
@@ -10,10 +10,14 @@ fn main() {
     }
     for b in io::stdin().bytes() {
         let c = b.unwrap() as char;
+        if c.is_control() {
+            writeln!(&mut out,"Binary: {0:08b} ASCII: {0:#03}\r\n", c as u8).unwrap();
+        } else {
+            writeln!(&mut out, "Binary: {0:08b} ASCII: {0:#03} Charactor: {1:#?}\r\n", c as u8, c).unwrap();
+        }
         if c ==  'q' {
             break;
         }
-        writeln!(&mut out ,"{}\n", c).unwrap();
     }
     if let Err(e) = disable_raw_mode() {
         writeln!(&mut out, "failed to stop a row mod\r\n").unwrap();
