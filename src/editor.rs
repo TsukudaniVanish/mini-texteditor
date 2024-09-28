662ffdf7188ed1c77dd4b9aa72bcd45a305090bf
Y.Naito <tsukudani710@gmail.com>
2024-09-28 11:56:51 +0900

    refactor: use crossterm's features to polish codes

--- src/editor.rs
+++ src/editor.rs
@@ -1,31 +1,35 @@
+use crossterm::event::{read, Event::Key, KeyCode::Char};
 use crossterm::terminal::{disable_raw_mode, enable_raw_mode};
-use std::io::{self, Read, Result, Write};
+use std::io::{self, Result, Write};
 
 pub struct Editor {}
 
 impl Editor {
     pub fn new() -> Self {
-        Self{}
+        Self {}
     }
 
     pub fn run(&self) -> Result<()> {
         let mut out = io::stdout();
         enable_raw_mode()?;
-        for b in io::stdin().bytes() {
-            let c = b.unwrap() as char;
-            if c.is_control() {
-                writeln!(&mut out, "Binary: {0:08b} ASCII: {0:#03}\r\n", c as u8)?;
-            } else {
-                writeln!(
-                    &mut out,
-                    "Binary: {0:08b} ASCII: {0:#03} Charactor: {1:#?}\r\n",
-                    c as u8, c
-                )?;
-            }
-            if c == 'q' {
-                break;
+        loop {
+            match read() {
+                Ok(Key(event)) => {
+                    write!(&mut out, "{:?}\r\n", event)?;
+                    match event.code {
+                        Char(c) => {
+                            if c == 'q' {
+                                break;
+                            }
+                        }
+                        _ => (),
+                    }
+                }
+                Err(e) => write!(&mut out, "Err: {:?}\r\n", e)?,
+                _ => (),
             }
         }
+
         disable_raw_mode()?;
         return Ok(());
     }
