bae3bf47778f70393c6f4e354c4a4f14865f12af
Y.Naito <tsukudani710@gmail.com>
2024-09-28 12:54:35 +0900

    feat: clear screen

--- src/editor.rs
+++ src/editor.rs
@@ -1,5 +1,7 @@
-use crossterm::event::{read, Event::Key, KeyCode::Char, KeyEvent, KeyModifiers};
-use crossterm::terminal::{disable_raw_mode, enable_raw_mode};
+use crossterm::cursor::MoveTo;
+use crossterm::event::{read, Event, Event::Key, KeyCode::Char, KeyEvent, KeyModifiers};
+use crossterm::execute;
+use crossterm::terminal::{disable_raw_mode, enable_raw_mode, Clear, ClearType};
 use std::io::{self, Result, Write};
 
 pub struct Editor {
@@ -13,35 +15,58 @@ impl Editor {
 
     pub fn run(&mut self) -> Result<()> {
         let mut out = io::stdout();
-        self.repl(&mut out)
+        self.initialize(&mut out)?;
+        let result = self.repl(&mut out);
+        Self::terminate()?;
+        result
+    }
+
+    fn initialize<O: Write>(&self, out: &mut O) -> Result<()> {
+        enable_raw_mode()?;
+        Self::clear_screen(out)
+    }
+
+    fn terminate() -> Result<()> {
+        disable_raw_mode()
+    }
+
+    fn clear_screen<O: Write>(out: &mut O) -> Result<()> {
+        execute!(out, Clear(ClearType::All))?;
+        execute!(out, MoveTo(0, 0))
     }
 
     fn repl<O: Write>(&mut self, out: &mut O) -> Result<()> {
         enable_raw_mode()?;
         loop {
-            if let Key(KeyEvent {
-                code,
-                modifiers,
-                kind,
-                state,
-            }) = read()?
-            {
-                write!(
-                    out,
-                    "Code: {code:?} Modifiers: {modifiers:?} Kind: {kind:?} State: {state:?}\r\n"
-                )?;
-                match code {
-                    Char('q') if modifiers == KeyModifiers::CONTROL => {
-                        self.shuld_quit = true;
-                    }
-                    _ => (),
-                }
-            }
+            let event = read()?;
+            self.evaluate_event(&event);
+            self.referesh_screen(out)?;
             if self.shuld_quit {
                 break;
             }
         }
-        disable_raw_mode()?;
+        Ok(())
+    }
+
+    fn evaluate_event(&mut self, event: &Event) {
+        if let Key(KeyEvent {
+            code, modifiers, ..
+        }) = event
+        {
+            match code {
+                Char('q') if *modifiers == KeyModifiers::CONTROL => {
+                    self.shuld_quit = true;
+                }
+                _ => (),
+            }
+        }
+    }
+
+    fn referesh_screen<O: Write>(&self, out: &mut O) -> Result<()> {
+        if self.shuld_quit {
+            Self::clear_screen(out)?;
+            write!(out, "Goodbye. \r\n")?;
+        }
         Ok(())
     }
 }
